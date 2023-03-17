3613ce21b592866ddb3280ffdbaaa149fddd8748
iebgit <iebdev84@gmail.com>
2023-03-17 13:58:10 -0400

    add files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
